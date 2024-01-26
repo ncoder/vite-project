1e71914bc0d3f04f4b1920a1fd1367c3f7acdc53
ncoder <ncoder@embarkrpg.com>
2024-01-26 14:12:58 -0800

    example use of drag-drop with react-dockable-ts

--- src/Demo.tsx
+++ src/Demo.tsx
@@ -1,5 +1,22 @@
 import { useState } from 'react';
 import Dockable, { Widget } from '../lib/react-dockable-ts/src';
+import { Draggable, DraggingStyle, Droppable, NotDraggingStyle } from 'react-beautiful-dnd';
+
+const getItemStyle = (isDragging: boolean, draggableStyle: DraggingStyle | NotDraggingStyle | undefined) => ({
+  // change background colour if dragging
+  background: isDragging ? 'lightgreen' : 'grey',
+
+  // styles we need to apply on draggables
+  ...draggableStyle
+});
+
+// a little style helper for the list
+const getListStyle = (isDraggingOver: boolean) => ({
+  background: isDraggingOver ? 'lightblue' : 'lightgrey',
+  padding: 8,
+  width: 250,
+});
+
 
 function Demo() {
   const [state, setState] = useState({
@@ -19,6 +36,58 @@ function Demo() {
     ],
   });
 
+  const [data, setData] = useState({
+    items: [
+      {
+        id: '1',
+        location: 'MyComponentA',
+        content: 'First',
+      },
+      {
+        id: '2',
+        location: 'MyComponentB',
+        content: 'Second',
+      },
+      {
+        id: '3',
+        location: 'MyComponentC',
+        content: 'Third',
+      },
+    ]
+  })
+
+  const DroppableItems = ({ parentId }: { parentId: string }) => {
+    return (<Droppable droppableId="droppable">
+      {(provided, snapshot) => (
+        <div
+          {...provided.droppableProps}
+          ref={provided.innerRef}
+          style={getListStyle(snapshot.isDraggingOver)}
+        >
+          {data.items.filter(item=> item.location == parentId).map((item, index) => (
+            <Draggable key={item.id} draggableId={item.id} index={index}>
+              {(provided, snapshot) => (
+                <div
+                  ref={provided.innerRef}
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                  style={getItemStyle(
+                    snapshot.isDragging,
+                    provided.draggableProps.style
+                  )}
+                >
+                  {item.content}
+                </div>
+              )}
+            </Draggable>
+          ))}
+          {provided.placeholder}
+        </div>
+      )}
+    </Droppable>)
+  }
+
+
   return (
     <div
       style={{
@@ -30,18 +99,20 @@ function Demo() {
         initialState={state.panels}
         onUpdate={workspace => setState({ panels: workspace })}
         spacing={3}
-        // themeClass={css.theme}
+      // themeClass={css.theme}
       >
         <Widget id="MyComponentA" title="Component A">
-          <div>test content</div>
+          <DroppableItems parentId="MyComponentA"></DroppableItems>
         </Widget>
 
         <Widget id="MyComponentB" title="Component B">
-          <div>test content</div>
+          <DroppableItems parentId="MyComponentB"></DroppableItems>
+
         </Widget>
 
         <Widget id="MyComponentC" title="Component C">
-          <div>test content</div>
+          <DroppableItems parentId="MyComponentC"></DroppableItems>
+
         </Widget>
       </Dockable>
     </div>
